7dfd973ef2a0ab88e6970e0c5c9289e147085e09
= <marcelroed@gmail.com>
2019-02-27 13:09:33 +0100

    Calculate slope and intercept of line through points

--- src/helpers.py
+++ src/helpers.py
@@ -21,6 +21,14 @@ def add_to(vec_to, vec_from):
     for i in range(len(vec_to)):
         vec_to[i] += vec_from[i]
 
+def _slope_and_intercept(line):
+    p1, p2 = line
+    if p1[0] == p2[0]:
+        # Infinite slope
+        return None, None
+    else:
+        slope = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        return slope, p1[1] - slope * p1[0]
 
 def rot_center(image, rect, angle):
     """rotate an image while keeping its center"""
