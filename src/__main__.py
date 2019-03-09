33ce106005438bbc2a60955188841adaa7b05f17
= <marcelroed@gmail.com>
2019-03-09 12:01:12 +0100

    Get response from game, rebind to home row friendly keybindings, enable escape to quit.

--- src/__main__.py
+++ src/__main__.py
@@ -4,10 +4,10 @@ from sys import exit
 from src.racecar_game import Game, Controls
 
 controls = {
-    pygame.K_UP: Controls.FRONT,
-    pygame.K_LEFT: Controls.LEFT,
-    pygame.K_RIGHT: Controls.RIGHT,
-    pygame.K_DOWN: Controls.BACK
+    pygame.K_i: Controls.FRONT,
+    pygame.K_j: Controls.LEFT,
+    pygame.K_l: Controls.RIGHT,
+    pygame.K_k: Controls.BACK
 }
 
 
@@ -23,15 +23,18 @@ def player_game():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT, {}))
+
         dt = clock.tick(60) / 1000
-        print(dt)
+        # print(dt)
         pressed = pygame.key.get_pressed()
         control = []
         for key, action in controls.items():
             if pressed[key]:
                 control.append(action)
-        game.act(control, dt)
-        game.render()
+        response = game.act(control, dt)
+        game.render(dt)
 
 
 if __name__ == '__main__':
