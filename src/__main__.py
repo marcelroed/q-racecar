030b70abd93d4229b9b971052355d5ab6831e5a6
= <marcelroed@gmail.com>
2019-03-11 00:30:52 +0100

    Main function for running AI

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,10 @@
 from sys import exit
 
+import numpy as np
 import pygame
 from pygame.time import Clock
 
+from src.agent import CarAgent
 from src.racecar_game import Game, Controls
 
 controls = {
@@ -15,7 +17,8 @@ controls = {
 
 def main():
     # cProfile.run('player_game()', sort='tottime')
-    player_game()
+    # player_game()
+    ai_game()
 
 
 def player_game():
@@ -30,7 +33,6 @@ def player_game():
                 pygame.event.post(pygame.event.Event(pygame.QUIT, {}))
 
         dt = clock.tick(60) / 1000
-        # print(dt)
         pressed = pygame.key.get_pressed()
         control = []
         for key, action in controls.items():
@@ -39,6 +41,38 @@ def player_game():
         response = game.act(control, dt)
         print(response)
         game.render(dt)
+
+
+def ai_game():
+    game = Game(init_graphics=True)
+    clock = Clock()
+    state_shape, action_shape = [10, 4]
+    agent = CarAgent(state_shape, action_shape)
+    state = np.reshape(unpack_response(game.car.sense())[0], (1, state_shape))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT, {}))
+        dt = clock.tick(60) / 1000  # Only used to force at most 60 fps
+        dt = 1 / 60
+        actions = agent.act(state)
+        next_state, reward, done = unpack_response(agent.act(actions, dt))
+        reward = reward if not done else -10
+        agent.remember(state, actions, reward, next_state, done)
+        state = next_state
+
+
+def unpack_response(response):
+    car = response.get('car', {})
+    done = car.pop('done', False)
+    reward = car.pop('reward', None)
+    state = list(car.values())
+    print(state)
+    return state, reward, done
+
 
 
 if __name__ == '__main__':
